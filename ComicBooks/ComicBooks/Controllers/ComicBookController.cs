a7ffbb9d15e5811bfb7820ebebab25c9314a1b21
TechStaff <TechStaff@LAB3-PC14>
2020-02-25 18:46:58 +0200

    Updated a model

--- ComicBooks/ComicBooks/Controllers/ComicBookController.cs
+++ ComicBooks/ComicBooks/Controllers/ComicBookController.cs
@@ -11,6 +11,18 @@ namespace ComicBooks.Controllers
        
         public ActionResult Detail()
         {
+
+
+            ViewBag.SeriesTitle = "The Amazing Spiderman";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>In this issue spider man is consumed by a dark viscious liquid...... <strong>Could it mean Venom is coming back<strong></p>";
+            ViewBag.Artists = new string[] {
+                "Script: Dan Middleton",
+                "Pencils: Shikparak Singh",
+                "Inks: Victor Shabalala",
+                "Colors: Mbali Shozi",
+                "Letters: Shareek Parker" };
+
             return View();
         }
     }
